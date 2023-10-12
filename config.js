2b45f14ac3fc18fc6b7556c74e6a32abd78374b3
IMMSY-BLAQ <79642356+IMMSY-BLAQ@users.noreply.github.com>
2023-10-13 00:00:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['+2348039873948', 'IMMSYBLAQ', true],
+  ['08039873948', 'IMMSYBLAQ', true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['+2348039873948','+2348039873948'] 
-global.prems = ['+2348039873948', '+2348039873948', '+2348039873948']
+global.mods = ['08039873948','08039873948'] 
+global.prems = ['08039873948', '08039873948', '08039873948']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
